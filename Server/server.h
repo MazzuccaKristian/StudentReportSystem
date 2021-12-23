be6d09536195ff621d759df95ac8ced47c8702d0
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-23 18:25:43 +0100

    "SocketHandler" header included.

--- Server/server.h
+++ Server/server.h
@@ -1,8 +1,12 @@
 #include <fstream>
+#include <iostream>
+
+#include "./../SocketHandler/SocketHandler.h"
 
 using std::ifstream;
 using std::ofstream;
 
 #define RECORDS "records.txt"
+#define BUFFER_SIZE 10
 
 void ArchiveSetup();
