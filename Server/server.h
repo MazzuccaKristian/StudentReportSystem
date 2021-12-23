94c6f6fc1f3a9078f09d0c40dc807817d08b0d7f
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-23 19:11:38 +0100

    "LoginUser" and "SearchInArchive" defined.

--- Server/server.h
+++ Server/server.h
@@ -1,12 +1,18 @@
 #include <fstream>
 #include <iostream>
+#include <string.h>
 
 #include "./../SocketHandler/SocketHandler.h"
 
 using std::ifstream;
 using std::ofstream;
+using std::string;
 
 #define RECORDS "records.txt"
 #define BUFFER_SIZE 10
 
 void ArchiveSetup();
+
+bool LoginUser(string data);
+
+bool SearchInArchive(string key, string value);
