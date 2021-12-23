cf4a7faa8cf4d4be786c0bf32f515a3439a40da4
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-23 19:12:32 +0100

    "LoginUser" & "SearchInArchive" added. Prototype.

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,6 +1,7 @@
 #include "server.h"
 
-int main(){
+int main()
+{
     bool isRunning = true;
     int serverSocket, newSocket;
     struct sockaddr_storage serverStorage;
@@ -11,27 +12,96 @@ int main(){
     ArchiveSetup();
 
     serverSocket = ServerSocketSetup();
-
     addr_size = sizeof(serverStorage);
-    newSocket = accept(serverSocket, (struct sockaddr*)&serverStorage, &addr_size);
 
-    if(recv(newSocket, socketBuffer, BUFFER_SIZE, 0) < 0){
-        perror("RECV");
-    }else{
+    while (isRunning)
+    {
+        newSocket = accept(serverSocket, (struct sockaddr *)&serverStorage, &addr_size);
 
+        if (recv(newSocket, socketBuffer, BUFFER_SIZE, 0) < 0)
+        {
+            perror("RECV");
+        }
+        else
+        {
+            char option = socketBuffer[0];
+            if(option == 'x'){
+                string convertedBuffer = socketBuffer;
+                string loginData = convertedBuffer.substr(convertedBuffer.find_first_of("|") + 1);
+                if(LoginUser(loginData)){
+                    // Here if the user is in archive. Login OK
+                }else{
+                    // Here if the user entered wrong credentials. Login must be repeated.
+                }
+            }
+        }
     }
 }
 
-void ArchiveSetup(){
+/**
+ * @brief Setup for archive file. Creation only, no initalization.
+ * 
+ */
+void ArchiveSetup()
+{
     ifstream records(RECORDS);
-    if(records.is_open()){
+    if (records.is_open())
+    {
         // File exists. No further setup needed.
         records.close();
-    }else{
+    }
+    else
+    {
         // File doesn't exists. Creation needed.
         ofstream recordsSetup(RECORDS);
-        if(recordsSetup.is_open()){
+        if (recordsSetup.is_open())
+        {
             recordsSetup.close();
         }
     }
+}
+
+/**
+ * @brief Handle user's login with archive file.
+ * 
+ * @param data Username and password concatenated.
+ * @return true if these values have been found in archive
+ * @return false 
+ */
+bool LoginUser(string data){
+    string username, password;
+    bool found = false;
+    username = data.substr(0, data.find_first_of("|"));
+    password = data.substr(data.find_first_of("|") + 1);
+    if(SearchInArchive("username:", username)){
+        if(SearchInArchive("password:", password)){
+            found = true;
+        }
+    }
+    return found;
+}
+
+/**
+ * @brief Search if a key-value pair exists in archive.
+ * 
+ * @param key wanted key
+ * @param value wanted value
+ * @return true if key-value pair exists
+ * @return false 
+ */
+bool SearchInArchive(string key, string value){
+    bool found = false;
+    ifstream records(RECORDS);
+    if(records.is_open()){
+        string readedKey, readedValue;
+        while(records >> readedKey >> readedValue){
+            if(readedKey.compare(key) == 0){
+                if(readedValue.compare(value) == 0){
+                    found = true;
+                }
+            }
+        }
+        records.close();
+    }
+    return found;
 }
