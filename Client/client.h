a4884118249eef80bff2dc51b065685c85b6c05c
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-26 17:29:30 +0100

    BUFFER_SIZE updated.

--- Client/client.h
+++ Client/client.h
@@ -4,6 +4,8 @@
 //TODO: fix include of SocketHandler. Tips: includePath over relative path to file.
 #include "./../SocketHandler/SocketHandler.h"
 
+#define BUFFER_SIZE 20
+
 using std::cout;
 using std::endl;
 using std::string;
