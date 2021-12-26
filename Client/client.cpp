3d498ef34a7273e45e8895743dc5310e359a3ee3
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-26 17:46:34 +0100

    "ShowMainMenu" and "GetUserChoice" added.

--- Client/client.cpp
+++ Client/client.cpp
@@ -29,12 +29,27 @@ int main(){
             }
         }else{
             //TODO: starting now: show main menu; get user's choice; run related option.
-            
+            ShowMainMenu();
+            int choice = GetUserChoice();
+            switch(choice){
+                case 0:
+                    cout << "Program ended. Please, wait..." << endl;
+                    isRunning = false;
+                    break;
+
+                case 1:
+                    
+            }
         }
     }
     return 0;
 }
 
+/**
+ * @brief Get username and password from user.
+ * 
+ * @return string Formatted string.
+ */
 string LoginHandler(){
     string username, password, userData;
     cout << "Please, enter your credentials." << endl;
@@ -44,4 +59,36 @@ string LoginHandler(){
     getline(cin, password);
     userData = "x|" + username + "|" + password;
     return userData;
+}
+
+/**
+ * @brief Show main menu and related options to user.
+ * 
+ */
+void ShowMainMenu(){
+    cout << "MAIN MENU" << endl;
+    cout << "1. Create new record;" << endl;
+    cout << "2. Search student's record;" << endl;
+    cout << "3. Show student's record;" << endl;
+    cout << "0. Exit." << endl;
+}
+
+/**
+ * @brief Get and validate user's choice.
+ * 
+ * @return int Validate choice.
+ */
+int GetUserChoice(){
+    bool isValid = false;
+    int choice;
+    while(!isValid){
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if(choice <= 3 && choice >= 0){
+            isValid = true;
+        }else{
+            cout << "Choice not allowed. Please, try again..." << endl;
+        }
+    }
+    return choice;
 }
